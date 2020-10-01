e09f8b79b17a7d72df3c9ac7a27d582c30f360ad
kk741847 <72181282+kk741847@users.noreply.github.com>
2020-10-01 09:35:47 +0530

    Update firstprogram.py

--- firstprogram.py
+++ firstprogram.py
@@ -2,5 +2,5 @@ g = 9.8
 h = float(input("Enter the value of height in meters : "))
 t = ((2*h)/g)**0.5
 v = (2*g*h)**0.5
-printf("Time taken by body to reach the ground : ",t, "s")
-printf("Speed of body just before hitting the ground : ",v, "m/s")
+print("Time taken by body to reach the ground : ",t, "s")
+print("Speed of body just before hitting the ground : ",v, "m/s")
